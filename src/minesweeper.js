53c71c32ab10ef336d3c61c2930f5e72ef66ea7f
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-01-14 14:28:19 -0600

    dynamicly create game board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,45 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	let board = [];
+
+	for (let i = 0; i < numberOfRows; i++) {
+		let row = [];
+		for (let j = 0; j < numberOfColumns; j++) {
+			row.push(' ');
+		}
+	board.push(row);
+	}
+	return board;
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	const board = [];
+	for (let i = 0; i < numberOfRows; i++) {
+		let row = [];
+		for (let j = 0; j < numberOfColumns; j++) {
+			row.push(null);
+		}
+	board.push(row);
+	}
+
+	let numberOfBombsPlaced = 0;
+	while (numberOfBombs > numberOfBombsPlaced) {
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++;
+		//this method can place bombs on top of bombs.
+		//fix with if statement
+	}
+	return board;
+}
+
 const printBoard = (board) => {
-	console.log('Current board:')
-	console.log(board[0].join(' | '));
-	console.log(board[1].join(' | '));
-	console.log(board[2].join(' | '));
-};
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 
-const board = [[' ',' ',' '],
-							 [' ',' ',' '],
-							 [' ',' ',' ']];
-
-printBoard(board);
-
-board[0][1] = '1';
-
-printBoard(board); 
-
-board[2][2] = 'B';
-
-printBoard(board);
+let playerBoard = generatePlayerBoard(9, 9);
+let bombBoard = generateBombBoard(9, 9, 5);
+console.log('Player Board: ')
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
